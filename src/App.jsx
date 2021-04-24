20a6d8b947ef027634185bcc3b5840e0ce780276
Rachel <racheljay42@gmail.com>
2021-04-23 20:46:17 -0400

    add axios, pinged api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+// api url https://pokeapi.co/api/v2/pokemon/ditto
+
 import './App.css';
 
 function App() {
+
+  useEffect(()=> {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
+    .then(res => {
+      const data = res.data;
+      console.log("data", data)
+   
+    })
+  })
+
+
   return (
     <div className="App">
       Hello
