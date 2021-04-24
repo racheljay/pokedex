72e9a86dd905e1e8a701df275d7b0fba030b4f2e
Rachel <racheljay42@gmail.com>
2021-04-23 22:02:06 -0400

    add bootstrap, fix api link

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,33 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 // api url https://pokeapi.co/api/v2/pokemon/ditto
+// https://pokeapi.co/api/v2/pokemon?limit=150
 
 import './App.css';
 
 function App() {
 
   useEffect(()=> {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/1`)
-    .then(res => {
-      const data = res.data;
-      console.log("data", data)
-   
-    })
-  })
+
+    const data = []
+
+    
+      axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+      .then(res => {
+        const data = res.data.results;
+        console.log(data)
+      })
+    
+
+    // console.log(data)
+
+  },[])
 
 
   return (
-    <div className="App">
-      Hello
-    </div>
+    <div className="container">
+  <h1>Pokedex</h1>
+  </div>
   );
 }
 
